081a4451841b848b7c33829572cbb568576a3c38
Nivida jain <nividajain107@gmail.com>
2026-07-01 22:23:47 +0530

    first commit

---
+++ aquatrackbackend/aquatrack-backend/src/main/java/com/aquatrack/aquatrack_backend/AquatrackBackendApplication.java
@@ -0,0 +1,13 @@
+package com.aquatrack.aquatrack_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AquatrackBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AquatrackBackendApplication.class, args);
+	}
+
+}
---
+++ aquatrackbackend/aquatrack-backend/src/test/java/com/aquatrack/aquatrack_backend/AquatrackBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aquatrack.aquatrack_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AquatrackBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
